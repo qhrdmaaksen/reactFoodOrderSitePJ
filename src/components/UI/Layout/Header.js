98d3350fa4c0f47ad0fb8f5ad47b4cf0990eeb09
vitamin777 <rlaalsdn8@gmail.com>
2022-08-03 18:04:38 +0900

    style: cart button component styling
    -cart icon 추가
    -cart button styling
    -cartIcon svg 추가
    -수량 badge 아이콘 추가
    -cartButton styling
    
    -아이콘 적용 완료 버튼은 아직 비활성화
    
    feat: cart button component 추가
    
    -헤더 부분 마이 카트 버튼 컴포넌트 추가 및 스타일 적용
    
    -App 적용 완료 버튼은 현재 미동작중

--- src/components/UI/Layout/Header.js
+++ src/components/UI/Layout/Header.js
@@ -1,13 +1,14 @@
 import React, {Fragment} from 'react';
 import classes from './Header.module.css' /*(내장 css 모듈 지원 사용 예정)*/
 import mealsIMG from '../../../assets/meals.jpg'
+import HeaderCartButton from './HeaderCartButton'
 
 const Header = (props) => {
 	return ( /*JSX 사용 시 루트 요소는 하나만 존재 !*/
 			<Fragment>
 				<header className={classes.header}>
 					<h1>vitamin777 리엑트 레스토랑</h1>
-					<button>Cart</button>
+					<HeaderCartButton>Cart</HeaderCartButton>
 				</header>
 				<div className={['main-image']}>
 					<img src={mealsIMG} alt='테이블가득음식이미지'/>
---
+++ src/components/UI/Layout/HeaderCartButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import CartIcon from '../Cart/CartIcon'
+import classes from './HeaderCartButton.module.css'
+
+const HeaderCartButton = (props) => {
+	return (
+			<button className={classes.button}>
+				{/*---카트 아이콘---*/}
+				<span className={classes.icon}>
+					<CartIcon />
+				</span>
+				<span>
+					내 장바구니
+				</span>
+				{/*-----수량-----*/}
+				<span className={classes.badge}>3</span>
+			</button>
+	)
+}
+
+export default HeaderCartButton;
